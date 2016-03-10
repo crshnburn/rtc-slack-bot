138557c65691fe80e6d5046a70ad8845af4e8dea
Andrew Smithson <smithson@uk.ibm.com>
2016-03-10 13:24:45 +0000

    Add botkit and listener for RTC based messages

--- app.js
+++ app.js
@@ -21,6 +21,21 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+var Botkit = require('botkit')
+var controller = Botkit.slackbot({
+  debug: false
+})
+
+var bot = controller.spawn({
+  token: "xoxb-25814718499-sXqQ27waMvKPtS67uozQQ1mR"
+}).startRTM()
+
+controller.hears(['(task|story|epic|defect) (\d*)'],'ambient',function(bot, message){
+  var matches = message.text.match(/(task|story|epic|defect) (\d*)/i)
+  var id = matches[2]
+  bot.reply(message, "https://jazz306.hursley.ibm.com:9443/ccm/resource/itemName/com.ibm.team.workitem.WorkItem/"+id)
+})
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
 
