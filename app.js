799b5b84dfa83de3a162ea6f7b186cb9b38e1738
Andrew Smithson <smithson@uk.ibm.com>
2016-03-10 13:26:52 +0000

    Remove starter app

--- app.js
+++ app.js
@@ -4,19 +4,9 @@
 // node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
-
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
-
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
@@ -35,10 +25,3 @@ controller.hears(['(task|story|epic|defect) (\d*)'],'ambient',function(bot, mess
   var id = matches[2]
   bot.reply(message, "https://jazz306.hursley.ibm.com:9443/ccm/resource/itemName/com.ibm.team.workitem.WorkItem/"+id)
 })
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-
-	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
