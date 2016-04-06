7cc51609b97a61c79261cfa032050bf2ce234aa8
Andrew Smithson <smithson@uk.ibm.com>
2016-04-06 17:06:21 +0100

    Only post messages when there's a number

--- app.js
+++ app.js
@@ -25,18 +25,20 @@ var bot = controller.spawn({
 })
 
 controller.hears(['(task|story|epic|defect) (\d*)'],'ambient',function(bot, message){
-  var matches = message.text.match(/(task|story|epic|defect) (\d*)/ig)
+  var matches = message.text.match(/(task|story|epic|defect) (\d+)/ig)
   var attachments = [];
-  for(var i=0; i < matches.length; i++){
-    var parts = matches[i].split(" ")
-    var type = parts[0].toLowerCase()
-    var id = parts[1]
-    attachments.push({
-    	"fallback": matches[i],
-    	"color": "#16B8DF",
-    	"title_link": process.env.JAZZ_URI + "/resource/itemName/com.ibm.team.workitem.WorkItem/" + id,
-    	"title": type.charAt(0).toUpperCase() + type.slice(1) + " " + id
-    })
+  if (matches != undefined) {
+    for(var i=0; i < matches.length; i++){
+      var parts = matches[i].split(" ")
+      var type = parts[0].toLowerCase()
+      var id = parts[1]
+      attachments.push({
+      	"fallback": matches[i],
+      	"color": "#16B8DF",
+      	"title_link": process.env.JAZZ_URI + "/resource/itemName/com.ibm.team.workitem.WorkItem/" + id,
+      	"title": type.charAt(0).toUpperCase() + type.slice(1) + " " + id
+      })
+    }
   }
   if (attachments.length > 0) {
   	bot.reply(message, {
